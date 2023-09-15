d71ce4b71e7615febb8298c93bf9ee768ad6511e
SamikshaD1 <127653626+SamikshaD1@users.noreply.github.com>
2023-09-15 10:05:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 //your code here
-
+let arr = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
+let sam = /\the\b|\ban\b|\ba\b/g;
+let noarticlearr = [];
+for(let i=0;i<arr.length;i++){
+	let noarticle = arr[i].replace(sam,"");
+	noarticlearr.push(noarticle);
+	
+}
+noarticlearr.sort();
+console.log(noarticlearr);
